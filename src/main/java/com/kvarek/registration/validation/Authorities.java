27168f4d44cba2c4d56c4631e1e92a1bd5b5d286
MagdalenaKwarecka <kvarek@gmail.com>
2020-09-14 19:52:03 +0200

    update Authorities

--- src/main/java/com/kvarek/registration/validation/Authorities.java
+++ src/main/java/com/kvarek/registration/validation/Authorities.java
@@ -12,9 +12,28 @@ public class Authorities {
 
 
     public Authorities() {
-        this.noAuthoritiesPathArray= new String[]{"/saveCoach", "/login"};
-        this.coachAuthoritiesPathArray = new String[]{"/exercise/initial", "/person/saveAthlete"};
-        this.athleteAuthoritiesPathArray = new String[]{"/exercise/findAllByNameContaining", "/person/updateAthlete"};
+        this.noAuthoritiesPathArray= new String[]{
+                "registration/saveCoach",
+                "registration/login"};
+        this.coachAuthoritiesPathArray = new String[]{
+                "/exercise/initial",
+                "/exercise/save",
+                "/exercise/delete",
+                "/exercise/findAllSortedByName",
+                "/exercise/findAllByNameContaining",
+                "/exerciseExecution/save",
+                "/person/saveAthlete",
+                "/person/findByLoginOrEmail",
+                "/person/findAllByLastNameContaining",
+                "/round/save",
+                "/wod/save",
+                "/wodExecution/save",
+                "/wodExecution/findAllByAthlete"
+        };
+        this.athleteAuthoritiesPathArray = new String[]{
+                "/exercise/findAllByNameContaining",
+                "/person/updateAthlete",
+                "/wodExecution/update"};
     }
 
     public String[] getNoAuthoritiesPathArray() {
@@ -23,7 +42,6 @@ public class Authorities {
     public String[] getCoachAuthoritiesPathArray() {
         return coachAuthoritiesPathArray;
     }
-
     public String[] getAthleteAuthoritiesPathArray() {
         return athleteAuthoritiesPathArray;
     }
